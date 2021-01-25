a2233fdc1f987e6d290b12cbcb8cf29cdf4b9a23
Heise <70926968+Heise1990@users.noreply.github.com>
2021-01-25 10:42:42 -0600

    added axes labels

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -73,3 +73,20 @@ chartGroup.append("g")
         .attr("opacity", ".5");
 
 });
+
+// // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - chartMargin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Healthcare (%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + chartMargin.top + 30})`)
+      .attr("class", "axisText")
+      .text("In Poverty (%)")
+  	  .catch(function(error) {
+    console.log(error)
+  });
