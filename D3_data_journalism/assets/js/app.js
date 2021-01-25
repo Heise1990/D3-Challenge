cd4b31db3d0ae80bdbfa36403e2b2c656b7d1ac8
Heise <70926968+Heise1990@users.noreply.github.com>
2021-01-25 10:32:58 -0600

    added circles to chart, need to add text within

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -22,8 +22,54 @@ let svg = d3.select("#scatter")
 
 //append chart to scatter div
 
+let chartGroup = svg.append("g")
+.attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
+	//import the data
 
-	//import the data to be parsed
+d3.csv("data.csv").then(function(chartData){
 
-	//parse data
+// Cast string data as a number using the unary + operator
+
+chartData.forEach(function(stringData){
+
+	stringData.poverty = +stringData.poverty;
+	stringData.healthcare = +stringData.healthcare;
+});
+
+// hard-code scales
+
+let xLinearScale = d3.scaleLinear()
+	.domain([0, 30])
+	.range([0,width]);
+
+let yLinearScale = d3.scaleLinear()
+	.domain([0, 30])
+	.range([height, 0]);
+
+// create axes
+
+let yAxis = d3.axisLeft(yLinearScale);
+let xAxis = d3.axisBottom(xLinearScale);
+
+// set y to the y axis and x to the bottom of the chart
+
+chartGroup.append("g")
+.call(yAxis);
+
+chartGroup.append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(xAxis);
+
+  // add circles to chart
+    let circlesGroup = chartGroup.selectAll("circle")
+        .data(chartData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", "10")
+        .attr("fill", "lightgreen")
+        .attr("opacity", ".5");
+
+});
